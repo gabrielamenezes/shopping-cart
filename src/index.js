32149beb509827fabd9c06a891955e9c1b9863b6
Gabriela Menezes <gabimenezess10@gmail.com>
2023-10-29 20:51:17 -0300

    feat: react router config

--- src/index.js
+++ src/index.js
@@ -1,28 +1,45 @@
-import { ThemeProvider, createTheme } from '@mui/material/styles'
-import { StyledEngineProvider } from '@mui/material/styles'
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Login from 'pages/Login';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { StyledEngineProvider } from "@mui/material/styles";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Login from "pages/Login";
+import Feira from "pages/Feira";
+import Carrinho from "pages/Carrinho";
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#2A9F85'
+      main: "#2A9F85",
     },
     secondary: {
-      main: '#FF7070'
+      main: "#FF7070",
     },
+  },
+});
+const router =  createBrowserRouter([
+  {
+    path: "/",
+    element: <Login/>
+  },
+  {
+    path: "/feira",
+    element: <Feira/>
+  },
+  {
+    path: "/carrinho",
+    element: <Carrinho/>
   }
-})
+])
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
       <ThemeProvider theme={theme}>
-        <Login />
+        <RouterProvider router={router} />
       </ThemeProvider>
     </StyledEngineProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
