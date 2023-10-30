dc92cd45c615339d19073dc45503e9f8bf144fed
Gabriela Menezes <gabimenezess10@gmail.com>
2023-10-30 11:00:59 -0300

    refector: dividindo responsabilidades do provider

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,10 @@
 import Carrinho from "pages/Carrinho";
 import Feira from "pages/Feira";
 import Login from "pages/Login";
-import { UsuarioContext } from 'common/context/Usuario'
+import {UsuarioProvider } from 'common/context/Usuario'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-import { useState } from "react";
 
 const Router = () => {
-    const [nome, setNome] = useState('')
-    const [saldo, setSaldo] = useState(0)
-    
     const router =  createBrowserRouter([
         {
           path: "/",
@@ -24,9 +20,9 @@ const Router = () => {
         }
       ])
   return (
-    <UsuarioContext.Provider value={{nome, setNome, saldo, setSaldo}}>
+    <UsuarioProvider>
         <RouterProvider router={router} />
-    </UsuarioContext.Provider>
+    </UsuarioProvider>
   )
 }
 
