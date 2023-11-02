2b1c53a60535095645434e42c010d531c7e8640d
Gabriela Menezes <gabimenezess10@gmail.com>
2023-11-02 19:57:16 -0300

    feat: add logic to disabled button and navigate function

--- src/pages/Feira/NavBar/index.js
+++ src/pages/Feira/NavBar/index.js
@@ -4,13 +4,15 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import IconButton from '@mui/material/IconButton';
 import Badge from '@mui/material/Badge';
 import { useCarrinhoContext } from 'common/context/Carrinho';
+import { useNavigate } from 'react-router-dom';
 
 export default function NavBar() {
   const {quantidadeDeProdutos} = useCarrinhoContext();
+  const navigate = useNavigate();
   return (
     <Nav>
       <Logo />
-      <IconButton disabled={quantidadeDeProdutos === 0}>
+      <IconButton disabled={quantidadeDeProdutos === 0} onClick={() => navigate('/carrinho')}>
         <Badge
           color="primary"
           badgeContent={quantidadeDeProdutos}
