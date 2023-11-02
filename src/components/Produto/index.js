8309da6f4fb2309275bead2e5df1cb8dd33f0b84
Gabriela Menezes <gabimenezess10@gmail.com>
2023-11-01 21:26:47 -0300

    feat: add products to shop cart

--- src/components/Produto/index.js
+++ src/components/Produto/index.js
@@ -1,40 +1,44 @@
-import { Container } from './styles';
-import { memo } from 'react';
-import IconButton from '@mui/material/IconButton';
-import AddIcon from '@mui/icons-material/Add';
-import RemoveIcon from '@mui/icons-material/Remove';
+import { Container } from "./styles";
+import { memo, useContext, useState } from "react";
+import IconButton from "@mui/material/IconButton";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import { CarrinhoContext } from "common/context/Carrinho";
 
+function Produto({ nome, foto, id, valor, unidade }) {
+  const {carrinho, setCarrinho} = useContext(CarrinhoContext)
 
-function Produto({
-  nome,
-  foto,
-  id,
-  valor,
-  unidade
-}) {
+  const adicionarProduto = (novoProduto) =>  {
+    const temOProduto = carrinho.some(itemDoCarrinho => itemDoCarrinho.id === novoProduto.id)
+    if(!temOProduto) {
+      novoProduto.quantidade = 1;
+      return setCarrinho(carrinhoAnterior => 
+        [...carrinhoAnterior, novoProduto])
+    }
+
+    setCarrinho(carrinhoAnterior => carrinhoAnterior.map(itemDoCarrinho => {
+      if(itemDoCarrinho.id === novoProduto.id) itemDoCarrinho.quantidade += 1;
+      return itemDoCarrinho
+    }))
+  }
   return (
-      <Container>
-        <div>
-          <img
-            src={`/assets/${foto}.png`}
-            alt={`foto de ${nome}`}
-          />
-          <p>
-            {nome} - R$ {valor?.toFixed(2)} <span>Kg</span>
-          </p>
-        </div>
-        <div>
-          <IconButton
-            color="secondary"
-          >
-            <RemoveIcon />
-          </IconButton>
-          <IconButton>
-            <AddIcon />
-          </IconButton>
-        </div>
-      </Container>
-  )
+    <Container>
+      <div>
+        <img src={`/assets/${foto}.png`} alt={`foto de ${nome}`} />
+        <p>
+          {nome} - R$ {valor?.toFixed(2)} <span>Kg</span>
+        </p>
+      </div>
+      <div>
+        <IconButton color="secondary">
+          <RemoveIcon />
+        </IconButton>
+        <IconButton onClick={() => adicionarProduto({nome, foto, id, valor})}>
+          <AddIcon />
+        </IconButton>
+      </div>
+    </Container>
+  );
 }
 
-export default memo(Produto)
+export default memo(Produto);
--- src/pages/Carrinho/index.js
+++ src/pages/Carrinho/index.js
@@ -1,12 +1,10 @@
 import { Button, Snackbar, InputLabel } from '@mui/material';
 import MuiAlert from '@mui/lab/Alert';
-import { useContext, useState } from 'react';
+import { useState } from 'react';
 import { Container, Voltar, TotalContainer, PagamentoContainer} from './styles';
-import { UsuarioContext } from 'common/context/Usuario';
 
 function Carrinho() {
   const [openSnackbar, setOpenSnackbar] = useState(false);
-  const usuario = useContext(UsuarioContext)
   return (
     <Container>
       <Voltar />
